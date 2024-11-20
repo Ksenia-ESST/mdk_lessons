44c2d9c77e1137088ac150fdcc93e61293486896
Ksenia-ESST <kburdina298@gmail.com>
2024-11-20 09:26:20 +0500

    Add files via upload

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtWidgets
 import main
 
 class MyWindow(QtWidgets.QWidget):
@@ -6,45 +6,31 @@ class MyWindow(QtWidgets.QWidget):
         QtWidgets.QWidget.__init__(self)
         self.ui = main.Ui_Form()
         self.ui.setupUi(self)
-        self.ui.push.clicked.connect(self.conver)
 
-        
-        
-        self.ui.line.setValidator(QtGui.QDoubleValidator())
-        self.ui.line.setValidator(
-            QtGui.QRegExpValidator(QtCore.QRegExp("[0-9]+[.][0-9]")))
-        
-        self.ui.line2.setValidator(QtGui.QRegExpValidator(QtCore.QRegExp("")))
+        self.ui.push1.clicked.connect(self.Text)
+        self.ui.push2.clicked.connect(self.Int)
+        self.ui.push3.clicked.connect(self.Double)
+        self.ui.push4.clicked.connect(self.Item)
 
-    
-    
-    def conver(self):
-        c = 0
-        a = float(self.ui.Iz.text())
-        if self.ui.Km1.isChecked() and self.ui.Km2.isChecked():
-            c = round(a, 2)
-        if self.ui.Km1.isChecked() and self.ui.M2.isChecked():
-            c = round((a * 1000), 2)
-        if self.ui.Km1.isChecked() and self.ui.Sm2.isChecked():
-            c = round((a * 100000), 3)
+    def Text(self):
+        text, ok = QtWidgets.QInputDialog.getText(window, "Заголовок", "Подсказка", echo = 0 )
+        if ok:
+            self.ui.line1.setText(str(text))
 
+    def Int(self):
+        c_chislo, ok = QtWidgets.QInputDialog.getInt(window, "Заголовок", "Подсказка", min = 0, max = 10, step = 1 )
+        if ok:
+            self.ui.line1.setText(str(c_chislo))
 
-        if self.ui.M1.isChecked() and self.ui.Km2.isChecked():
-            c = round((a*100), 2)
-        if self.ui.M1.isChecked() and self.ui.M2.isChecked():
-            c = round((a), 2)
-        if self.ui.M1.isChecked() and self.ui.Sm2.isChecked():
-            c = round((a / 1000), 2)
+    def Double(self):
+        v_chislo, ok = QtWidgets.QInputDialog.getDouble(window, "Заголовок", "Подсказка", min = 0, max = 10, decimals = 3 )
+        if ok:
+            self.ui.line1.setText(str(v_chislo))
 
-        
-        if self.ui.Sm1.isChecked() and self.ui.Km2.isChecked():
-            c = round((a* 100000), 3)
-        if self.ui.Sm1.isChecked() and self.ui.M2.isChecked():
-            c = round((a*100), 2)
-        if self.ui.Sm1.isChecked() and self.ui.Sm2.isChecked():
-            c = round(a, 2)
-
-        self.ui.line2.setText(str(c))
+    def Item(self):
+        item, ok = QtWidgets.QInputDialog.getItem(window, "Заголовок", "Подсказка", ["Пункт 1", "Пункт 2", "Пункт 3"], current = 0, editable = False)
+        if ok:
+            self.ui.line1.setText(str(item))
 
 if __name__ == "__main__":
     import sys

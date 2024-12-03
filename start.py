e1cfede983c23e568d436f78fc036f32d1e87ffc
Ksenia-ESST <kburdina298@gmail.com>
2024-12-03 16:10:38 +0500

    Add files via upload

--- start.py
+++ start.py
@@ -1,26 +1,30 @@
-from PyQt5 import QtCore, QtWidgets, QtMultimedia
-import main, os
+from PyQt5 import QtWidgets
+import main
 
 class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         self.ui = main.Ui_Form()
         self.ui.setupUi(self)
-        self.sndEffect = QtMultimedia.QSoundEffect()
-        self.ui.pushButton.clicked.connect(self.sound1)
-        self.ui.pushButton_2.clicked.connect(self.sound2)
+        self.ui.line2.textChanged.connect(self.deq)
+        self.ui.line.textChanged.connect(self.deq)
+        self.ui.push.clicked.connect(self.dan)  
 
-    def sound1(self):
-        fn = QtCore.QUrl.fromLocalFile(os.path.abspath("net.wav"))
-        self.sndEffect.setSource(fn)
-        self.sndEffect.setVolume(1)
-        self.sndEffect.play()
-
-    def sound2(self):
-        fn = QtCore.QUrl.fromLocalFile(os.path.abspath("2.wav"))
-        self.sndEffect.setSource(fn)
-        self.sndEffect.setVolume(0.5)
-        self.sndEffect.play()
+    def deq(self):
+        try:    
+            a = int(self.ui.line2.text())
+            b = int(self.ui.line.text())
+            c = a*b
+            if a is not None and b  is not None:
+                self.ui.label_2.setText(f"Амортизация налогового учета = {str(c)}")
+        except:
+            self.ui.label_2.setText(f"Амортизация налогового учета = ")
+    def dan(self):
+        number = self.ui.line3.text()
+        avtor = self.ui.line4.text()
+        date = self.ui.line5.text()
+        cvv = self.ui.line6.text()
+        self.ui.label3_2.setText(f"Данные: {number} {avtor} {date} {cvv}")
 
 if __name__ == "__main__":
     import sys

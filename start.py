14e4529fdcd327616a1cfc1fd9d51b1263a19e56
Ksenia-ESST <kburdina298@gmail.com>
2024-12-03 16:11:40 +0500

    Add files via upload

--- start.py
+++ start.py
@@ -1,34 +1,32 @@
 from PyQt5 import QtWidgets
-import main
+import main, main2
 
 class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         self.ui = main.Ui_Form()
         self.ui.setupUi(self)
-        self.ui.line2.textChanged.connect(self.deq)
-        self.ui.line.textChanged.connect(self.deq)
-        self.ui.push.clicked.connect(self.dan)  
+        self.ui.pushButton.clicked.connect(self.open)
 
-    def deq(self):
-        try:    
-            a = int(self.ui.line2.text())
-            b = int(self.ui.line.text())
-            c = a*b
-            if a is not None and b  is not None:
-                self.ui.label_2.setText(f"Амортизация налогового учета = {str(c)}")
-        except:
-            self.ui.label_2.setText(f"Амортизация налогового учета = ")
-    def dan(self):
-        number = self.ui.line3.text()
-        avtor = self.ui.line4.text()
-        date = self.ui.line5.text()
-        cvv = self.ui.line6.text()
-        self.ui.label3_2.setText(f"Данные: {number} {avtor} {date} {cvv}")
+    def open(self):
+        vt.show()
+
+class VtoroeWindow(QtWidgets.QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
+        self.ui = main2.Ui_Form()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.open2)
+
+    def open2(self):
+        a= float(self.ui.lineEdit.text())
+        window.ui.label.setText(str(a+(a*0.15)))
+        vt.close()
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MyWindow()
+    vt = VtoroeWindow()
     window.show()
     sys.exit(app.exec_())
